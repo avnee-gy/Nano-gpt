84ccc708f586c9f37a1df56d7754a1f1e3b42e75
avnee-gy <avneegoyal410@gmail.com>
2023-04-06 15:45:09 +0530

    parameters changed

--- train.py
+++ train.py
@@ -10,9 +10,9 @@ eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
-n_head = 4
-n_layer = 4
+n_embd = 384
+n_head = 6
+n_layer = 6
 dropout = 0.2
 # ------------
 
